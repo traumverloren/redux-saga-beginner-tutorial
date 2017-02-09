d8ac3a461fc8c8bd51fc399c7e8f973dde0b4e37
Stephanie Nemeth <traumverloren@gmail.com>
2017-02-09 09:46:43 +0100

    add redux-saga middleware, hello world

--- main.js
+++ main.js
@@ -3,11 +3,23 @@ import "babel-polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleWare from 'redux-saga';
+
+import { helloSaga } from './sagas';
 
 import Counter from './Counter'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+const __REDUX_DEVTOOLS_EXTENSION__ = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+const sagaMiddleWare = createSagaMiddleWare();
+const store = createStore(
+  reducer,
+  __REDUX_DEVTOOLS_EXTENSION__ && __REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(sagaMiddleWare)
+)
+
+sagaMiddleWare.run(helloSaga);
 
 const action = type => store.dispatch({type})
 
